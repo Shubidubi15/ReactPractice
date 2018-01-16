aa5cae94a9350e525b8cc48f63ed8176bed61ef0
Erick <erick.123467@yahoo.com>
2018-01-15 22:40:55 -0600

    Se creó el primer componente

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -1,14 +1,15 @@
 import React, { Component, createElement } from 'react';
 import './App.css';
+import Person from './Components/Person/Person'
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>HOI!!</h1>
-    //   </div>
-    // );
-    return React,createElement('div', null, 'h1', 'HOI!!! Im Temmie...!');
+    return (
+      <div className="App">
+        <h1>HOI!!</h1>
+        <Person/>
+      </div>
+    )
   }
 }
 
