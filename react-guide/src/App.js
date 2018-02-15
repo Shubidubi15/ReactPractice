d8437a36ccb9473c6eaf4ea6952e8b55a222a2ca
Erick <adahir.rosas@gmail.com>
2018-02-14 21:10:01 -0600

    Styling Person component and button

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -32,12 +32,21 @@ class App extends Component {
     }
 
   render() {
+    const style = {
+      backgroundColor: 'steelblue',
+      font: 'inherit',
+      border: '1px solid black',
+      padding: '8px',
+      color: 'white',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>HOI!!</h1>
         <Person change={this.changedHandler} name = {this.state.persons[0].name} game = {this.state.persons[0].game}></Person>
         <Person clic={() => this.switchNameHandler('Richi')} name = {this.state.persons[1].name} game = {this.state.persons[1].game}></Person>
-        <button onClick = {this.switchNameHandler.bind(this, 'Richard')}>Change name</button>
+        <button onClick = {this.switchNameHandler.bind(this, 'Richard')} style={style}>Change name</button>
       </div>
     )
   }
--- react-guide/src/Components/Person/Person.js
+++ react-guide/src/Components/Person/Person.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <p onClick = {props.clic}>I'm {props.name}, and I love to play {props.game}</p>
             <input type="text" onChange={props.change} value={props.name}/>
         </div>
